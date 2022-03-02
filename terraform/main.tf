e6ac19827f6e51b4e91f0db687e9949b9a3cff04
Sardorbek Imomaliev <imomaliev@hey.com>
2022-03-02 23:42:18 +0700

    add terraform files for managing repository

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+module "github" {
+  source = "git@github.com:imomaliev/terraform-registry.git//modules/github"
+
+  token               = var.github_token
+  project_name        = "Library Template"
+  project_description = "Template for creating library repository"
+  topics              = ["template", "terraform", "pre-commit", "editorconfig"]
+  is_template         = true
+}
